56491ca3ce55bfcc2dc43ee07428fb83a66486aa
sumrak667 <the_azamat07@mail.ru>
2024-09-12 11:30:47 +0500

    Add files via upload

---
+++ asd.java
@@ -0,0 +1,6 @@
+public class asd {
+    public static void main(String[] args) {
+        // Print "Hello, World!" to the console
+        System.out.println("Hello, Shaler, How are you!");
+    }
+}
